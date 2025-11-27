cdaae4397b6c3cc196f8ecce59590a311b2ce3b0
Ashilpa Darrel Bumika <ashilpa.darrel@gmail.com>
2025-11-27 22:03:09 +0700

    feat: add UI index

---
+++ components/ui/index.tsx
@@ -0,0 +1,13 @@
+import Experience from "@/components/ui/Experience";
+import ExternalLink from "@/components/ui/ExternalLink";
+import IconButton from "@/components/ui/IconButton";
+import NavBar from "@/components/ui/NavBar";
+import Project from "@/components/ui/Project";
+
+export {
+    Experience,
+    ExternalLink,
+    IconButton,
+    NavBar,
+    Project
+}
