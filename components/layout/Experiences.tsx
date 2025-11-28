5ed9a3da7a3f73347320f7575545f264a3955ad3
Ashilpa Darrel Bumika <ashilpa.darrel@gmail.com>
2025-11-28 23:04:06 +0700

    feat: add timline ui using ant design library

--- components/layout/Experiences.tsx
+++ components/layout/Experiences.tsx
@@ -1,5 +1,23 @@
 import { EXPERIENCES } from "@/constant";
 import { Experience } from "@/components/ui";
+import { Timeline } from "antd"
+
+const experiences = EXPERIENCES.map((experience) => (
+    {
+        content: (
+            <Experience
+                position={experience.position}
+                company={experience.company}
+                year={experience.year}
+                arrangement={experience.arrangement}
+                type={experience.type}
+            />
+        ),
+        icon: (
+            <div className="min-w-6 h-6 bg-black rounded-full border-4 border-background" />
+        )
+    }
+));
 
 export default function Experiences() {
     return (
@@ -11,16 +29,10 @@ export default function Experiences() {
                 II. EXPERIENCES
             </h2>
             <div className="flex flex-col gap-8">
-                {EXPERIENCES.map((experience) => (
-                    <Experience
-                        key={experience.position + experience.company}
-                        position={experience.position}
-                        company={experience.company}
-                        year={experience.year}
-                        arrangement={experience.arrangement}
-                        type={experience.type}
-                    />
-                ))}
+                <Timeline
+                    items={experiences}
+                    variant="filled"
+                />
             </div>
         </section>
     )
